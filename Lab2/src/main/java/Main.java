5929d25d0d74b7888f23ed4fdb98e0452024536d
zzz <jalowskidaw@gmail.com>
2019-12-19 13:19:00 +0000

    Adding gitignore file

--- Lab2/src/main/java/Main.java
+++ Lab2/src/main/java/Main.java
@@ -2,13 +2,13 @@ public class Main
 {
     public static void main(String [] args)
     {
-        /*try
+        try
         {
-
+            ParameterReader parReader = new ParameterReader(args);
         }
         catch(ErrorException e)
         {
             System.out.println(e);
-        }*/
+        }
     }
 }
